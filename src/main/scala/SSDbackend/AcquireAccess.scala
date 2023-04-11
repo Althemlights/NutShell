fc6b9287878ea5c89f8957a16f316da4b606b478
FuWenyi <2393394711@qq.com>
2023-04-11 10:26:08 +0800

    commit storebuffer to difftest

--- src/main/scala/SSDbackend/AcquireAccess.scala
+++ src/main/scala/SSDbackend/AcquireAccess.scala
@@ -214,7 +214,7 @@ sealed class AcquireAccess(edge: TLEdgeOut)(implicit val p: Parameters) extends 
   io.resp.bits.cmd := SimpleBusCmd.readBurst
   io.resp.bits.user.zip(io.req.bits.user).map { case (o, i) => o := i }
 
-  Debug(io.mem_grantAck.fire && addr.index === 0x3e.U, "[DCache Miss] Addr:%x Tag:%x AcquireBlock:%x Data:%x\n", addr.asUInt, addr.tag, state === s_grantD, io.mem_grantAck.bits.data.asUInt)
+  //Debug(io.mem_grantAck.fire && addr.index === 0x3a.U, "[DCache Miss] Addr:%x Tag:%x AcquireBlock:%x Data:%x\n", addr.asUInt, addr.tag, state === s_grantD, io.mem_grantAck.bits.data.asUInt)
 }
 
 sealed class IAcquireAccess(edge: TLEdgeOut)(implicit val p: Parameters) extends ICacheModule{
